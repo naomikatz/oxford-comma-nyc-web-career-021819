1459208b790747692f50b67b3c88198a6509e584
Naomi Katz <naykatz@gmail.com>
2019-02-16 20:08:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 2 
+    string = ""
+    string << array.join(" and ")
+    elsif array.length == 3
+    string = ""
+    third_element = array.pop
+    string << array.join(", ") + ", and " + third_element
+  else array.length > 
+  string = ""
+  string << array.join(", ")
+  end
 end
